1e1000c5e79081a4ca362e14550539a07fe939ee
Abhay Vishwakarma <av385735@gmail.com>
2023-10-30 11:53:16 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SecBlog Manager',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
